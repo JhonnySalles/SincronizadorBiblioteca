afcb5dc8c5b562357c76bcb592a5db7c0ab52147
ElPsy <43912638+JhonnySalles@users.noreply.github.com>
2026-08-30 08:54:31 -0300

    Criado instalador para o programa.

--- build_release.bat
+++ build_release.bat
@@ -13,5 +13,24 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo.
-echo [SUCESSO] Publicacao em Release concluida com sucesso na pasta Executavel!
+echo =========================================
+echo Gerando Instalador MSI com WiX Toolset v4...
+echo =========================================
+
+dotnet build SyncLib.Installer\SyncLib.Installer.wixproj -c Release
+
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo [ERRO] A geracao do instalador MSI falhou!
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+echo Copiando instalador para a pasta Compilado...
+if not exist "Compilado" mkdir Compilado
+copy /Y SyncLib.Installer\bin\x64\Release\pt-BR\SyncLibSetup.msi .\Compilado\SyncLibSetup.msi > nul
+
+echo.
+echo [SUCESSO] Publicacao e Instalador MSI gerados com sucesso na pasta Compilado!
 pause
